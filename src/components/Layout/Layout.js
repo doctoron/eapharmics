fc9786854a50821c8085cfd1a9e5938b76d8b7fd
doctoron <ron.antonio@me.com>
2019-07-15 22:48:28 -0400

    lodash fix

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const layout = (props) => {
-    <div>
-        Nav,Menu,Login
-    </div>
-}
+import Aux from '../../hoc/Aux';
+
+const layout = (props) => (
+    <Aux>
+        <div>Navbar, Sidedrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
 
 export default layout;
