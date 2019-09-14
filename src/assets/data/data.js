4c8e0d604ee3499e382b8c49b054b7b06c1cc578
doctoron <ron.antonio@me.com>
2019-09-14 01:16:25 -0400

    Added fonts library

---
+++ src/assets/data/data.js
@@ -0,0 +1,97 @@
+
+num_titles = 9;
+for(var i=1; i<10; i++)
+{	
+	ttl[i] = 'eSTAR';
+	
+	
+	switch(i)
+	{
+		case 1:
+			links[1] = 'index.html';
+			desc[1]	= 'We are the developers of affordable innovative quality control data management software for stability and annual product review tracking.' +
+						'For the last fifteen years, we have provided our clients with tools for data management, evaluation, and report generation that emphasize.....';
+			keys[1] = 'Electronic Stability Testing Analyses & Reporting';
+			break;
+		case 2:
+			links[2] = 'contact.html';
+			desc[2]	= '<p>1113 Old Freehold Road<br>Toms River, NJ 08753<br>Phone/Fax (732) 244-6694<br>info@microcontrolsolutions.com</p><p>6121 Froggatt Street<br>Orlando, FL 32835<br>Phone/Fax (407) 578-2007<br>info@enviroapps.com</p>';
+			keys[2] = 'Phone,Fax,EAPHARMICS, CONTACT, new jearsy, orlando, new jearsy office, orlando office';
+			break;
+		case 3:
+			links[3] = 'about.html';
+			desc[3]	= 'EAPHARMICS has successfully demonstrated using current technologies in various applications including, but not limited to pharmaceutical, environmental, automobile,' +
+						'bio informatics, food, safety, geographical and water management industries.......';
+			keys[3] = 'Enviroapps, Microsontrolsolutions, eapharmics, Enviroapps INC, about';
+			break;
+		case 4:
+			links[4] = 'software.html';
+			desc[4]	= 'To meet the stability requirements ' +
+                    'set forth by FDA, EAPHARMICS has developed the eSTAR Stability ' +
+                    'System software package that handles \'all the requirements ' +
+                    'of a full-scale stability program. Sample management, report ' +
+                    'generation, and statistical evaluations are made available ' +
+                    'through an intuitive user interface that allows both experienced ' +
+                    'and occasional user the same level of comfort. The system ' +
+                    'also maintains multilevel security that ensures \'21 CFR part ' +
+                    '\'11 compliance.';
+			keys[4] = 'stability software, stability, software, 21 cfr, fda, 21 cfr part II, 21 cfr part 2';
+			break;
+		case 5:
+			links[5] = 'services.html';
+			desc[5]	= 'Online Support System Service';
+			keys[5] = 'online, systems, supports, services';
+			break;
+		case 6:
+			links[6] = 'cfr.html';
+			desc[6]	= '';
+			keys[6] = '21 cfr, cfr part 2, fda';
+			break;
+		case 7:
+			links[7] = 'knowledgecenter.html';
+			desc[7]	= '';
+			keys[7] = 'online, systems, supports, services';
+			break;
+		case 8:
+			links[8] = 'impLinks.html';
+			desc[8]	= '';
+			keys[8] = 'online, systems, supports, services';
+			break;
+		case 9:
+			links[9] = 'label.html';
+			desc[9]	= '';
+			keys[9] = 'online, systems, supports, services';
+			break;
+	}
+	keys[i] = keys[i] + 'STABILITY SOFTWARE, FDA, EAPHARMICS, PHARMACEUTICAL, LABELS, BARCODE';
+	matched[i] = 0;
+}
+/*
+//ttl[1] = 'Stability Software'
+links[1] = 'software.html'
+desc[1] = ' iWeb Software is home to the webs finest Interactivity Applications.  Search engines, forums, surveys, and more. '
+keys[1] = 'iweb,iweb software,perl,software'
+matched[1]=0
+
+//ttl[2] = 'Stability Software'
+links[2] = 'index.html'
+desc[2] = 'Start your own Yahoo! Today.  Hyperseek powers over 2,000 internet search engines, directories, and portals.'
+keys[2] = 'search engine, search, hyperseek, yahoo'
+matched[2]=0
+
+//ttl[3] = 'Stability Software'
+links[3] = 'http://www.hyperseek.com/jackhammer/'
+desc[3] = 'Built on proven Hyperseek Technology, the Jackhammer System opens up a whole new world of profits and opportunity for your Search Engine presence.  Modeled after the very popular "Goto.com" system, Jackhammer allows webmasters listed within your search engine to determine their placement in search results and category listings by bidding on a "Per Click" price. Sites with the highest bid on the current search term or category will be listed first, and thereafter in order of the bid amount.'
+keys[3] = 'ppc,goto,pay per click,hyperseek'
+matched[3]=0
+/*
+num_titles = 3;
+//var alinks=["","about.html","software.html","cfr.html","contact.html","impLinks.html","index.html","knowledgecenter.html","label.html","services.html"];
+for(var i=1; i<3; i++)
+{	
+	ttl[i] = 'Stability Software';
+	links[i] = 'links';
+	desc[i] = 'desc';
+	keys[i]='keys';
+	matched[i]=0;
+}*/
--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Search = () => {
+const Search = (props) => {
   return (
   <div></div>
   );
