152db8edf760a5e3f1700f32d7c887cf2d1370b4
doctoron <ron.antonio@me.com>
2019-11-19 18:02:58 -0500

    Contact form MVP

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,79 +1,89 @@
 import React from 'react'
-import { Jumbotron, Container, Row, Col, Card, Table, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Container, Row, Col, Button, Card, CardBody, FormGroup, Label, Input, Form } from 'reactstrap';
 import './Contact.css';
 
-class Contact extends React.Component {
-    render () {
-        return (
-            <div className="contact" >
-                <Jumbotron fluid style={{backgroundColor: '#58b0c0'}} >
-                    <Container fluid >
-                        <Row>
-                            <Col xs="6">
-                                <Table>
-                                    <thead>
-                                        <tr>
-                                            <th styles={{ align: "left", backgroundolor: 'black' }}>EMAIL:</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <h6>
-                                            <tr>
-                                                <td>For General Inquiries: </td><td><a href='mailto:info@enviroapps.com?Subject=Hello%20again' target="_top"><p>info@enviroapps.com</p></a></td>
-                                            </tr>
-                                            <tr>
-                                                <td>For eStar related inquiries:</td><td><a href='mailto:info@eapharmics.com?Subject=Hello%20again' target="_top"><p>info@eapharmics.com</p></a></td>
-                                            </tr>
-                                            <tr>
-                                            </tr>
-                                            <td>For IT related inquiries:</td><td><a href='mailto:itinfo@enviroapps.com?Subject=Hello%20again' target="_top"><p>itinfo@enviroapps.com</p></a></td>
-                                        </h6>
-                                    </tbody>
-                                </Table>
-                            </Col>
-                            <Col xs="6">
-                                <Card body inverse style={{ backgroundColor: 'black', opacity: 0.6 }}><b>CONTACT FORM</b>
-                                    <Form inline style={{ fontSize: 11 }}>
-                                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                                            <Label for="interest">Your Interest With EnviroApps:</Label>
-                                            <Input style={{ fontSize: 10.5 }} type="select" name="select" id="interest">
-                                                <option>Environment</option>
-                                                <option>Technology</option>
-                                                <option>EAFarms</option>
-                                                <option>EPHARMICS</option>
-                                                <option>Zoomvin</option>
-                                            </Input>
-                                        </FormGroup>
+// class Contact extends React.Component {    
+const Contact = () => {
+    return (
+        <Container> 
+            <Row>
+                <Col md="4" >
+                    <Card >
+                        <CardBody >
+                            <Form className="Inq">
+                                <h3>Inquiries</h3>
+                                <Label for="GenInq">General: <a href="mailto:">info@enviroapps.com </a></Label>
+                                
+                                <Label for="eStarInq">eStar:<a href="mailto:">info@enviroapps.com</a> </Label>
 
-                                        <FormGroup>
-                                            <Label>Subject:</Label>
-                                            <Input style={{ fontSize: 11 }} type="subject" name="subject">
-                                            </Input>
-                                        </FormGroup>
+                                <Label for="ItInq">IT: <a href="mailto:">itinfo@enviroapps.com</a></Label>
+                                
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col md="4">
+                    <Card>
+                        <CardBody>
+                            <Form>
+                                <h3 className="text-center mb-4">Contact Us</h3>
+                                <FormGroup>
+                                    <Label for="multiInterest" className="grey-text">
+                                        Your Primary Interest With EnviroApps:</Label>
+                                    <Input type="select" name="multiInt" id="multiInterest">
+                                        <option>Environment</option>
+                                        <option>Technology</option>
+                                        <option>EAFarms</option>
+                                        <option>EAPharmics</option>
+                                        <option>ZoomVin</option>
+                                    </Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Subject:</Label>
+                                    <Input>
+                                    </Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Comments:</Label>
+                                    <Input type="textarea" name="comments" id="comments">
+                                    </Input>
+                                </FormGroup>
 
-                                        <FormGroup>
-                                            <Label>Email:</Label>
-                                            <Input style={{ fontSize: 11 }} type="email" name="email" placeholder="something@email.com">
-                                            </Input>
-                                        </FormGroup>
-                                        <FormGroup>
-                                            <Label>Comments:</Label>
-                                            <Input style={{ fontSize: 11 }} type="textarea" name="text" placeholder="Comments">
-                                            </Input>
-                                        </FormGroup>
-                                        <FormGroup>
-                                            <Button color="success" style={{ textAlign: 'center' }}>Submit</Button>
-                                        </FormGroup>
-                                    </Form>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
+                            </Form>
 
-                </Jumbotron>
-            </div>
-        )
-    }
-}
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col md="4">
+                    <Card>
+                        <CardBody>
+                            <Form>
+                                <h3 className="text-center mb-4">Register</h3>
+                                <Label for="EmailEx" className="grey-text">
+                                    Your email: </Label>
+                                <Input
+                                    type="email"
+                                    id="defaultFormLoginEmailEx"
+                                    className="form-control" />
+                                <br />
+                                <Label for="passwordEx" className="grey-text">
+                                    Your password </Label>
+                                <input
+                                    type="password"
+                                    id="passwordEx"
+                                    className="form-control" />
+                                <div className="text-center mt-4">
+
+                                    <Button color="primary" type="submit">Login</Button>
+                                </div>
+                            </Form>
+
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+
+        </Container>
+    );
+};
 export default Contact;
